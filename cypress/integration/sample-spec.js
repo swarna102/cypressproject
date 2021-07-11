5c21406d2766a9b90c1c515c9b596e7de05476cb
swarna102 <yenugusana@gmail.com>
2021-07-11 22:02:58 +0530

    second commit

---
+++ cypress/integration/sample-spec.js
@@ -0,0 +1,15 @@
+describe("my first test case", () => {
+    it("Does not do match", () => {
+        expect(true).to.equal(true);
+    });
+    it("visit site", () => {
+        cy.visit("http://localhost:3001/");
+        //cy.contains("swarna").click();
+        cy.contains("swarna").rightclick();
+        cy.url().should("include", "/");
+        //cy.pause();
+        //cy.debug();
+        cy.get("#root").find(".App");
+        cy.get("#root").find("a.App-link").should("contain", "swarna");
+    });
+});
